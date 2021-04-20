85907f022cd6cd509df1519c038c77683114f280
rlockard22 <60930604+rlockard22@users.noreply.github.com>
2021-04-20 14:46:33 -0400

    Add files via upload
    updated

--- statistics.py
+++ statistics.py
@@ -5,9 +5,9 @@ def stats():
     fp = "False Positives: 9                                                                                                 "
     fn = "False Negatives: 3                                                                                                 "
     tn = "True Negatives: 44                                                                                                 "
-    a = "Accuracy: 0.8537                                                                                                    "
-    p = "Precision: .8163                                                                                                    " 
-    r = "Recall: .9302                                                                                                       "
-    f1 = "F1 Score: .8695                                                                                                    "
+    a = "Accuracy:    .8750                                                                                                    "
+    p = "Precision:   .8163                                                                                                    " 
+    r = "Recall:      .9302                                                                                                       "
+    f1 = "F1 Score:    .8695"
     my_str = ms + blank + tp + blank + fp + blank + fn + blank + tn + blank + a + blank + p + blank + r + blank + f1
     return(my_str)
